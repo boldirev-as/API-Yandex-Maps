59dd43e996614627a62f3fc3baea0ab8087ffec5
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-02-12 20:11:51 +0500

    Полный поиск

--- Полный поиск.py
+++ Полный поиск.py
@@ -1,9 +1,10 @@
+import sys
 from io import BytesIO
 import requests
 from PIL import Image
 from second_file import get_size_toponym
 
-toponym_to_find = input()
+toponym_to_find = " ".join(sys.argv[1:])
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
